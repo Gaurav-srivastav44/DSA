3e973b55f9adfae7b1d30827cb478c5d5b2ee1dd
Gaurav Srivastav <gaurav.srivastav_cs23@gla.ac.in>
2025-09-28 19:22:41 +0530

    Time: 4 ms (56.69%) | Memory: 55.6 MB (54.09%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
@@ -6,10 +6,8 @@ class Solution {
         }
         int ans[][] = new int[m][n];
         int k = 0;
-        for (int i=0; i<m; i++) {
-            for (int j=0; j<n; j++) {
-                ans[i][j] = original[k++];
-            }
+        for (int i=0; i<l; i++) {
+            ans[i / n][i % n] = original[i];
         }
         return ans;
 
