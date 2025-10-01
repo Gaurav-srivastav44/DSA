13686860cdec321d4ca4a5914dde654e06178b50
Gaurav Srivastav <gaurav.srivastav_cs23@gla.ac.in>
2025-10-01 14:19:02 +0530

    Time: 3 ms (80.30%) | Memory: 45.4 MB (91.46%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -9,13 +9,13 @@ class Solution {
 
         for(int i=1; i<n; i++){
             if (ratings[i] > ratings[i - 1]) {
-                left[i] = Math.max(left[i], left[i - 1] + 1);
+                left[i] = left[i - 1] + 1;
             }
         }
 
         for(int i=n-2; i>=0; i--){
             if (ratings[i] > ratings[i + 1]) {
-                right[i] = Math.max(right[i], right[i + 1] + 1);
+                right[i] = right[i + 1] + 1;
             }
         }
 
