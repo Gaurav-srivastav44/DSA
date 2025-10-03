c33c03d418e7da56f3bdd1a2853daa67aba430c5
Gaurav Srivastav <gaurav.srivastav_cs23@gla.ac.in>
2025-10-03 11:35:23 +0530

    Time: 1 ms (48.12%) | Memory: 42.7 MB (83.82%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -18,3 +18,25 @@ class Solution {
         return res;
     }
 }
+/*ye problem Subsets generate karne ki hai. Main isko bitmasking technique se solve kar raha hoon.
+Total subsets 2^n hote hain, kyunki har element ko ya to include kar sakte hain ya exclude.
+Main har subset ko ek binary number se represent kar raha hoon:
+
+Bit 1 → element include
+Bit 0 → element exclude
+
+Pehle, 1 << nums.length se total combinations nikalte hain. Ye 2^n ke barabar hai.
+
+Fir outer loop i se har subset ka mask iterate karte hain.
+Inner loop j se har element check karte hain:
+
+(i & (1 << j)) > 0 → iska matlab jth element subset me include hoga.
+
+Har subset cur list me add karte hain, aur finally res me store kar dete hain.
+
+Dry Run Example, nums = [1,2]:
+i=0 → 00 → subset = []
+i=1 → 01 → subset = [1]
+i=2 → 10 → subset = [2]
+i=3 → 11 → subset = [1,2]
+*/
