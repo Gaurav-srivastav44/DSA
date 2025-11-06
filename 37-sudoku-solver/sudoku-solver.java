ca181aa55e5453ed8aaad41700f52c4d51bea9e9
Gaurav Srivastav <gaurav.srivastav_cs23@gla.ac.in>
2025-11-06 23:35:10 +0530

    Time: 374 ms (23.25%) | Memory: 42.8 MB (7.51%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,36 +1,35 @@
-class Solution{
-    public void solveSudoku(char[][] board){
+class Solution {
+    public void solveSudoku(char[][] board) {
         solve(board);
     }
-
-    private boolean solve(char[][] board){
-        for(int row=0; row<9; row++){
-            for(int col=0; col<9; col++){
-                if(board[row][col]=='.'){
-                    for(char c='1';c<='9';c++){
-                        if(isValid(board,row,col,c)){
-                            board[row][col]=c;
+    public boolean solve(char[][] board){
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[i][j] == '.'){
+                    for(char c='1'; c<='9'; c++){
+                        if(check(board,i,j,c)){
+                            board[i][j] = c;
                             if(solve(board)){
                                 return true;
                             }
-                            board[row][col]='.';
+                            board[i][j] = '.';
                         }
                     }
-                    return false;
+                    return  false;
                 }
             }
         }
         return true;
     }
 
-    private boolean isValid(char[][] board,int row,int col,char c){
-        for(int i=0;i<9;i++){
-            if(board[row][i]==c||board[i][col]==c){
+    public boolean check(char[][] board, int row, int col, char c){
+        for(int i=0; i<9; i++){
+            if(board[row][i] == c || board[i][col] == c){
                 return false;
             }
-            int boxRow=3*(row/3)+i/3;
-            int boxCol=3*(col/3)+i%3;
-            if(board[boxRow][boxCol]==c){
+            int boxr= 3*(row/3) + i/3;
+            int boxc= 3*(col/3) + i%3;
+            if(board[boxr][boxc] == c){
                 return false;
             }
         }
