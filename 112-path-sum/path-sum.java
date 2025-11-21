da7d1b5e7ab554e18a694c98c6484d7cb31b53b4
Gaurav Srivastav <gaurav.srivastav_cs23@gla.ac.in>
2025-11-21 11:46:12 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (12.83%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -15,12 +15,16 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
+        //agar koi value hi nahi hai to target sum ke barabar kanbhi nahi hoga
         if(root ==  null)return false;
-        
+
+        //agar ek hi element hai (left or right nahi hai) to check karenge ki uski value target ke barabar hai to true nahi to false
         if(root.left == null && root.right == null){
             if(targetSum == root.val)return true;
             else return false;
         }
+
+        //fir recursive call karenge left  or right ke liye leafnode tak jaenge  agar sum possibble hai to true
         return hasPathSum(root.left, targetSum-root.val)
                 ||
                 hasPathSum(root.right, targetSum-root.val);
