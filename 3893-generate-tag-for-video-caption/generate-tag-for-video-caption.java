7430f0162a6cbeff774adcfae14fe6500cb1efea
Gaurav Srivastav <gaurav.srivastav_cs23@gla.ac.in>
2025-12-22 23:01:41 +0530

    Time: 10 ms (49.10%) | Memory: 47.7 MB (14.08%) - LeetSync

---
+++ 3893-generate-tag-for-video-caption/generate-tag-for-video-caption.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String generateTag(String caption) {
+        String[] words = caption.trim().split("\\s+");
+        StringBuilder sb = new StringBuilder("#");
+
+        if (words.length == 0) return "#";
+
+        for (int i = 0; i < words.length; i++) {
+            String w = words[i].toLowerCase();
+
+            if (i == 0) {
+                sb.append(w);
+            } else {
+                sb.append(Character.toUpperCase(w.charAt(0)));
+                if (w.length() > 1)
+                    sb.append(w.substring(1));
+            }
+
+            if (sb.length() >= 100) break;
+        }
+
+        if (sb.length() > 100) {
+            return sb.substring(0, 100);
+        }
+        else {
+            return sb.toString();
+        }
+
+    }
+}
