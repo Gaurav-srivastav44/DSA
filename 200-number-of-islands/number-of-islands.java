e6b8f2e2f19b926e49ec1685ed7e960cbfd062fe
Gaurav Srivastav <gaurav.srivastav_cs23@gla.ac.in>
2025-12-30 10:31:49 +0530

    Time: 3 ms (86.74%) | Memory: 52.4 MB (30.68%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -2,6 +2,7 @@ class Solution {
     public int numIslands(char[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
+
         int c=0;
 
         for(int i=0; i<n; i++){
@@ -14,17 +15,15 @@ class Solution {
         }
         return c;
     }
-    void dfs(char[][] grid, int i, int j){
-        int n=grid.length;
-        int m=grid[0].length;
-
-        if(i<0 || j<0 || i>=n || j>=m || grid[i][j] =='0')return;
-
+    public void dfs(char[][] grid, int i, int j){
+        if(i<0 || j<0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0'){
+            return;
+        }
         grid[i][j] = '0';
 
         dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
         dfs(grid,i,j+1);
+        dfs(grid,i-1,j);
         dfs(grid,i,j-1);
     }
 }
