24671b8b24ef3d86050396ae8c88f4c5f08d804a
Gaurav Srivastav <gaurav.srivastav_cs23@gla.ac.in>
2025-12-30 14:59:56 +0530

    Time: 17 ms (51.04%) | Memory: 47.9 MB (17.36%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -3,27 +3,30 @@ class Solution {
         int n = grid.length;
         if (grid[0][0]==1 || grid[n-1][n-1]==1) return -1;
 
-        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
 
         Queue<int[]> q = new LinkedList<>();
         boolean[][] vis = new boolean[n][n];
-
+        
+        //in queue we store row, column and distance
         q.add(new int[]{0,0,1});
         vis[0][0] = true;
 
+        int[][] moves = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
         while(!q.isEmpty()){
+            //for all 8 direction 
             int[] cur = q.poll();
             int x = cur[0];
             int y = cur[1];
             int d = cur[2];
 
             if(x==n-1 && y==n-1) return d;
-            for(int[] k : dir){
-                int nx = x + k[0];
-                int ny = y + k[1];
+            for(int[] m : moves){
+                //make new x and y
+                int nx=x+m[0];
+                int ny=y+m[1];
 
-                if(nx>=0 && ny>=0 && nx<n && ny<n 
-                   && grid[nx][ny]==0 && !vis[nx][ny]){
+                //check if valid
+                if(nx>=0 && ny>=0 && nx<n && ny<n && grid[nx][ny]==0 && !vis[nx][ny]){
                     vis[nx][ny] = true;
                     q.add(new int[]{nx, ny, d+1});
                 }
