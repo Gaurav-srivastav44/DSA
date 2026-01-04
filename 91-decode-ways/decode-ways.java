300c10169b2f9565d3e1df0c9c30e1fd2248999b
Gaurav Srivastav <gaurav.srivastav_cs23@gla.ac.in>
2026-01-04 16:09:18 +0530

    Time: 1 ms (85.61%) | Memory: 42.9 MB (87.52%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,24 +1,26 @@
 class Solution {
     public int numDecodings(String s) {
         int n = s.length();
-        if (n == 0 || s.charAt(0) == '0') return 0;
+        if(n==0 || s.charAt(0) == '0')return 0;
+        int[] dp = new int[n+1];
 
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
+        dp[0]=1;
+        dp[1]=1;
 
-        for (int i=2; i<=n; i++) {
-            char c1 = s.charAt(i - 1);
-            char c2 = s.charAt(i - 2);
+        for(int i=2; i<=n; i++){
+            int c1 = s.charAt(i-1) - '0';
+            int c2 = s.charAt(i-2) - '0';
 
-            if (c1 != '0')
-                dp[i] += dp[i - 1];
-
-            int val = (c2-'0') * 10 + (c1-'0');
-            if (val >= 10 && val <= 26)
-                dp[i] += dp[i - 2];
+            if(c1 != 0){
+                dp[i] += dp[i-1];
+            }
+            int k = c2 *10 +c1;
+            if(k>=10 && k<=26){
+                dp[i] += dp[i-2];
+            }
         }
-
         return dp[n];
+
+
     }
 }
