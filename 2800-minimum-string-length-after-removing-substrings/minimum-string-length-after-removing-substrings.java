9bc15d03d1002831e71012312a3e6ed0434e33c6
Gaurav Srivastav <gaurav.srivastav_cs23@gla.ac.in>
2026-01-21 15:08:01 +0530

    Time: 6 ms (33.16%) | Memory: 46.7 MB (9.61%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -1,24 +1,12 @@
 class Solution {
     public int minLength(String s) {
-        Stack<Character> st = new Stack<>();
-
-        for(char i : s.toCharArray()){
-            if(!st.isEmpty()){
-                char top = st.peek();
-                if((top=='A' && i=='B') || (top=='C' && i=='D')){
-                    st.pop();
-                }
-                else{
-                    st.push(i);
-                    continue;
-                }
-            }
-            else{
-                st.push(i);
-            }
+        int len =-1;
+        while(s.length() != len){
+            len = s.length();
+            s = s.replace("AB", "");
+            s = s.replace("CD", "");
         }
 
-        int ans = st.size();
-        return ans;
+        return s.length();
     }
 }
