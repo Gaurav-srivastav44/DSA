6d872deb50d3654f7c32e2edbc73cba153b6a525
Gaurav Srivastav <gaurav.srivastav_cs23@gla.ac.in>
2026-01-21 15:11:45 +0530

    Time: 1 ms (99.79%) | Memory: 46.4 MB (75.66%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] freq = new int[26];
+
+        for(char i : magazine.toCharArray()){
+            freq[i - 'a']++;
+        }
+
+        for(char i : ransomNote.toCharArray()){
+            if(freq[i-'a']-- == 0)return false;
+
+        }
+        return true;
+    }
+}
