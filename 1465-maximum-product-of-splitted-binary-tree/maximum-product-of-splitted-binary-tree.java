7d257463884bb72f2801718f15d49d2349791b21
Gaurav Srivastav <gaurav.srivastav_cs23@gla.ac.in>
2026-02-16 11:40:05 +0530

    Time: 8 ms (38.45%) | Memory: 61.4 MB (30.31%) - LeetSync

--- 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.java
+++ 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.java
@@ -15,23 +15,27 @@
  */
 class Solution {
     public int maxProduct(TreeNode root) {
-        long total = bsum(root);
-        long[] ans = new long[1];
+        int total = bsum(root);
+        long ans[] = new long[1];
+
         dfs(root, total, ans);
         return (int)(ans[0] % 1000000007);
+
     }
     public int bsum(TreeNode root){
-        if(root == null) return 0;
-        return root.val
-               + bsum(root.left)
-               + bsum(root.right);
-        
+        if(root == null)return 0;
+        return root.val + bsum(root.left) + bsum(root.right);
     }
-    public long dfs(TreeNode root, long total, long[] max) {
-        if (root == null) return 0;
 
-        long curr = root.val + dfs(root.left, total, max) + dfs(root.right, total, max);
-        max[0] = Math.max(max[0], curr * (total - curr));
+    public long dfs(TreeNode root, int total, long[] ans){
+        if(root == null)return 0;
+
+        long left = dfs(root.left, total, ans);
+        long right = dfs(root.right, total, ans);
+        long curr = root.val + left + right;
+
+        ans[0] = Math.max(ans[0], curr *(total - curr)); 
         return curr;
+
     }
 }
