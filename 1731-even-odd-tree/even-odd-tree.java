5990ffd96255729f8079dbcaaf0d175ff4345dda
Gaurav Srivastav <gaurav.srivastav_cs23@gla.ac.in>
2026-02-20 12:46:32 +0530

    Time: 10 ms (83.69%) | Memory: 78 MB (66.11%) - LeetSync

---
+++ 1731-even-odd-tree/even-odd-tree.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isEvenOddTree(TreeNode root) {
+        int lvl =0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        TreeNode prev = null;
+
+        while(!q.isEmpty()){
+            int len = q.size();
+
+            for(int i=0; i<len; i++){
+                TreeNode curr = q.poll();
+
+                if(lvl%2==0){
+                    if(curr.val %2 == 0)return false;
+                    if(i != 0){
+                        if(curr.val <= prev.val)return false;
+                    }
+                }
+                else{
+                    if(curr.val %2 != 0)return false;
+                    if(i != 0){
+                        if(curr.val >= prev.val)return false;
+                    }
+                }
+                
+                prev = curr;
+                if(curr.left != null)q.add(curr.left);
+                if(curr.right != null)q.add(curr.right);
+            }
+            lvl++;
+
+        }
+        return true;
+
+    }
+
+    
+}
