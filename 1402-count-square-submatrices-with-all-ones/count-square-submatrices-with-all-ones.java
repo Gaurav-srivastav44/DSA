f80ca79bef1161127ef6fdb870486d0179f8ba9e
Gaurav Srivastav <gaurav.srivastav_cs23@gla.ac.in>
2026-03-03 15:34:10 +0530

    Time: 6 ms (84.90%) | Memory: 61.6 MB (76.63%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -4,13 +4,14 @@ class Solution {
         int n = matrix[0].length;
 
         int[][] dp = new int[m][n];
-        dp[0][0] = matrix[0][0];
-        for(int i=1; i<m; i++)dp[i][0] = matrix[i][0];
-        for(int i=1; i<n; i++)dp[0][i] = matrix[0][i];
 
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                if(matrix[i][j] == 1){
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 || j==0){
+                    dp[i][j] = matrix[i][j];
+                }
+
+                else if(matrix[i][j] == 1){
                     dp[i][j] = matrix[i][j] + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
                 }
                 else dp[i][j] = 0;
