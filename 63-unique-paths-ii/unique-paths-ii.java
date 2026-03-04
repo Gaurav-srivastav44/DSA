53bb35c7fbd2a713306846b2e3c1daaffa493330
Gaurav Srivastav <gaurav.srivastav_cs23@gla.ac.in>
2026-03-04 17:33:15 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (25.19%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -3,35 +3,22 @@ class Solution {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
         int[][] dp = new int[m][n];
-
-        if (obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1){
-            return 0;
-        }
-
-        for(int i=0;i<m;i++){
-            if(obstacleGrid[i][0] != 1){
-                dp[i][0] =1;
-            }
-            else break;
-        }
-        for(int j=0;j<n;j++){
-            if(obstacleGrid[0][j] != 1){
-                dp[0][j] =1;
-            }
-            else break;
-        }
-
-        int ans=0;
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                if(obstacleGrid[i][j] == 1){
-                    dp[i][j] = 0;
-                }
-                else{
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
-                }
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                dp[i][j]=-1;
             }
         }
-        return dp[m-1][n-1];
+        return helper(obstacleGrid, 0, 0, dp);
+    }
+    public int helper(int[][] grid, int i, int j, int[][] dp){
+        int m=grid.length;
+        int n=grid[0].length;
+        
+        if(i>=m || j>=n || grid[i][j] == 1)return 0;
+        if(i==m-1 && j==n-1)return 1;
+        if(dp[i][j] != -1)return dp[i][j];
+
+        return dp[i][j] = (helper(grid,i+1,j, dp) + helper(grid,i,j+1, dp));
+
     }
 }
