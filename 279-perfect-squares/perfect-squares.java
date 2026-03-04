6e9cd8946cde03898f3128a59aa69eefc75fbe43
Gaurav Srivastav <gaurav.srivastav_cs23@gla.ac.in>
2026-03-04 17:23:50 +0530

    Time: 62 ms (36.29%) | Memory: 44.9 MB (26.79%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return helper(n, dp);
+    }
+
+    public int helper(int n, int[] dp){
+        if(n==0)return 0;
+        if(dp[n] != -1)return dp[n];
+        
+        int min=n;
+        for(int i=1; i*i<=n; i++){
+            min = Math.min(min,1+ helper(n-i*i, dp));
+        }
+        return dp[n]=min;
+    }
+}
