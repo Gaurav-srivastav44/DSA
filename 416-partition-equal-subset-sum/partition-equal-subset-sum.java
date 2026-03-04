8a7c122548d33fa9be6fc39eefc2c6ad275fde6e
Gaurav Srivastav <gaurav.srivastav_cs23@gla.ac.in>
2026-03-04 18:25:44 +0530

    Time: 107 ms (11.87%) | Memory: 72.5 MB (36.22%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,33 +1,31 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int n = nums.length;
-        int total = 0;
-        for(int i : nums){
-            total += i;
-        }
+        int n=nums.length;
+        int sum=0; for(int i:nums)sum+=i;
+        if(sum%2 != 0)return false;
 
-        if(total %2 !=0) return false;
+        int target = sum/2;
+
+        int[][] dp = new int[n][target+1];
+        for(int i=0;i<n;i++)
+            Arrays.fill(dp[i],-1);
         
-        int sum =total/2;
-        boolean[][] dp = new boolean[n+1][total+1];
-        for(int  i=0; i<n; i++){
-            dp[i][0]=true;
+        return helper(0, nums, target, dp);
+    }
+    public boolean helper(int i, int[] nums, int target, int[][] dp){
+        if(i == nums.length || target<0)return(target == 0);
+
+        if(dp[i][target] != -1){
+            return (dp[i][target]==1);
         }
 
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=sum; j++){
-                
-                if(nums[i-1] <= j){
-                    dp[i][j] = dp[i-1][j] || dp[i-1][j- nums[i-1]];
-                }
+        if(target < 0)return false;
+        boolean skip = helper(i+1, nums, target, dp);
+        boolean pick = helper(i+1, nums, target-nums[i], dp);
 
-                else{
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
+        dp[i][target] = (skip||pick) ? 1 : 0;
 
-        return dp[n][sum];
+        return(skip || pick);
 
     }
 }
