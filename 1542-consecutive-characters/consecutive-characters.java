bc0dc3fbb67df84215e947dde9f2ee5abb74ae03
Gaurav Srivastav <gaurav.srivastav_cs23@gla.ac.in>
2026-03-06 14:16:02 +0530

    Time: 1 ms (100.00%) | Memory: 43.8 MB (11.86%) - LeetSync

---
+++ 1542-consecutive-characters/consecutive-characters.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxPower(String s) {
+        int ans=1;
+        int c=1;
+        for(int i=1; i<s.length(); i++){
+            if(s.charAt(i)==s.charAt(i-1)){
+                c++;
+            }
+            else c=1;
+            ans = Math.max(ans, c);
+        }
+        return ans;
+    }
+}
