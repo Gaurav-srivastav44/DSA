c8b6e96beeff1ba36a7509858af6d7a484cc6808
Gaurav Srivastav <gaurav.srivastav_cs23@gla.ac.in>
2026-03-06 22:26:04 +0530

    Time: 2 ms (85.77%) | Memory: 47.9 MB (80.49%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,15 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
-        int far = 0;
+        int far=0;
 
         for(int i=0; i<n; i++){
-            if(i >far){
-                return false;
-            }
-            far= Math.max(far, i+nums[i]);
+            if(i>far)return false;
+            far = Math.max(far, i+nums[i]);
+
         }
         return true;
-
     }
 }
