7c161ca9acbb8162396eed634af121d7a9bb9840
Gaurav Srivastav <gaurav.srivastav_cs23@gla.ac.in>
2026-03-09 21:08:19 +0530

    Time: 1 ms (43.90%) | Memory: 61.1 MB (84.46%) - LeetSync

---
+++ 189-rotate-array/rotate-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k=k%n;
+        reverse(0, n-1, nums);
+        reverse(0, k-1, nums);
+        reverse(k, n-1, nums);
+    }
+    public void reverse(int i, int j, int[] arr){
+        while(i<j){
+            int temp = arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+    }
+}
