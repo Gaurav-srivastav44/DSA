b8f0f92406bf9ddfc1b18be088494d2c8cb5f978
Gaurav Srivastav <gaurav.srivastav_cs23@gla.ac.in>
2026-03-11 16:03:20 +0530

    Time: 21 ms (43.09%) | Memory: 45.3 MB (99.07%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,26 +1,31 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        int n=nums.length;
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        List<List<Integer>> ans =  new ArrayList<>();
-        for(int l=0; l<n-2; l++){
-            if(l>0 && nums[l]==nums[l-1])continue;
-            for(int i=l+1; i<n-1; i++){
-                if(i>l+1 && nums[i]==nums[i-1])continue;
 
-                int j=i+1;
-                int k=n-1;
-                while(j<k){
-                    long sum = (long)nums[i]+nums[j]+nums[k]+nums[l];
+        for(int i=0; i<n-2; i++){
+            if(i>0 && nums[i]==nums[i-1])continue;
+            
+            for(int j=i+1; j<n-1; j++){
+                if(j>i+1 && nums[j] == nums[j-1])continue;
 
-                    if(sum<target)j++;
-                    else if(sum>target)k--;
-                    else{
-                        ans.add(Arrays.asList(nums[l],nums[i],nums[j],nums[k]));
-                        j++; k--;
-                        while(j<k && nums[j]==nums[j-1])j++;
-                        while(j<k && nums[k]==nums[k+1])k--;
+                int k=j+1;
+                int l=n-1;
+
+                while(k<l){
+                    long sum = (long)nums[i] + nums[j] +nums[k] + nums[l];
+
+                    if(sum==target){
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        k++;
+                        l--;
+
+                        while(k<l && nums[k] == nums[k-1])k++;
+                        while(k<l && nums[l] == nums[l+1])l--;
                     }
+                    else if(sum < target)k++;
+                    else l--;
                 }
             }
         }
