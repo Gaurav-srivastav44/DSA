eb82d68822056a06a83ed8d4e4e6bacfebefeca5
Gaurav Srivastav <gaurav.srivastav_cs23@gla.ac.in>
2026-03-13 16:26:33 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (39.88%) - LeetSync

--- 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.java
+++ 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.java
@@ -14,41 +14,18 @@
  * }
  */
 class Solution {
+    int sum=0;
     public TreeNode convertBST(TreeNode root) {
-        if(root == null)return null;
-        ArrayList<Integer> list = new ArrayList<>();
-        inorder(root, list);
-        Collections.sort(list);
-
-        int n = list.size();
-        int[] arr = new int[n];
-        for(int i=0; i<n; i++)arr[i] = list.get(i);
-
-        int[] suff = new int[n];
-        suff[n-1]=arr[n-1];
-        for(int i=n-2; i>=0; i--){
-            suff[i] = suff[i+1] + arr[i];
-        }
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<n; i++){
-            map.put(arr[i], suff[i]);
-        }
-        
-        return update(root, map);
-    }
-
-    public TreeNode update(TreeNode root, HashMap<Integer, Integer> map){
-        if(root == null)return root;
-        update(root.left, map);
-        root.val = map.get(root.val);
-        update(root.right, map);
+        traverse(root);
         return root;
     }
 
-    public void inorder(TreeNode root, ArrayList<Integer> list){
+    public void traverse(TreeNode root){
         if(root==null)return;
-        inorder(root.left, list);
-        list.add(root.val);
-        inorder(root.right, list);
+
+        traverse(root.right);
+        sum+= root.val;
+        root.val = sum;
+        traverse(root.left);
     }
 }
